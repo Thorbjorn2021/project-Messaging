799e9ad693c59eea77c9a7eeaac33f18b56e5263
Andreas Øihaugen <89156396+h598970@users.noreply.github.com>
2024-11-24 21:36:53 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,10 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-amqp")
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	implementation("org.springframework.boot:spring-boot-starter-json") // Spring Boot Jackson integration
+	implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2") // Core Databind library
+        implementation("com.fasterxml.jackson.core:jackson-annotations:2.15.2") // Annotations
+        implementation("com.fasterxml.jackson.core:jackson-core:2.15.2") // Core library
 	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.amqp:spring-rabbit-test")
